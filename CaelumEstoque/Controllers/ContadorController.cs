5c3fd7535fca532dbf6160a4600e4b4e884ea0d6
Marco Antonio <marcopandolfo577@gmail.com>
2019-02-12 18:26:33 -0200

    Session

---
+++ CaelumEstoque/Controllers/ContadorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {
+        public ActionResult Index()
+        {
+            object valorNaSession = Session["contador"];
+            int contador = Convert.ToInt32(valorNaSession);
+            contador++;
+            Session["contador"] = contador;
+            return View(contador);
+        }
+    }
+}
