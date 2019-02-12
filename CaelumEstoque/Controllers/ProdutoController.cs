5890ff19a52757c7eaf7bd037b24a47955be8a2f
Marco Antonio <marcopandolfo577@gmail.com>
2019-02-12 18:07:16 -0200

    Ajax

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -58,5 +58,14 @@ namespace CaelumEstoque.Controllers
             ViewBag.Produto = produto;
             return View();
         }
+
+        public ActionResult DecrementaQtd(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            produto.Quantidade--;
+            dao.Atualiza(produto);
+            return Json(produto);
+        }
     }
 }
